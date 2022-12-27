1ddccb6b9dce9f7762908e2a8aa5f3028162a37f
yellowsea <yunhai0644@163.com>
2022-12-27 17:22:07 +0800

    feat: 🎸 complete object module

---
+++ test/object/object.test.ts
@@ -0,0 +1,67 @@
+import { describe, expect, test } from 'vitest'
+import { deepClone, deepClone2, deepMerge } from '@/object'
+
+describe('object test', () => {
+  test('deep clone Object', () => {
+    const fixtures = [
+      {
+        target: 'normal',
+        type: 1,
+      },
+      {
+        target: null,
+        type: 1,
+      },
+      {
+        target: 'normal',
+        type: 0,
+      },
+      {
+        target: null,
+        type: 0,
+      },
+      {
+        target: [{ a: 2 }, { a: { b: 1 } }],
+        type: 1,
+      },
+      {
+        target: { a: /123/g, d: new Date() },
+        type: 0,
+      },
+    ]
+    fixtures.forEach((el) => {
+      const clone = el.type ? deepClone : deepClone2
+      expect(clone(el.target)).toEqual(el.target)
+    })
+  })
+
+  test('deepMerge', () => {
+    const fixtureOrigin = {
+      a: 1,
+      b: {
+        c: 2,
+      },
+      d: 4,
+    }
+
+    const fixturePatch = {
+      a: 2,
+      b: {
+        c: 3,
+      },
+      c: 5,
+    }
+
+    const fixtureExpected = {
+      a: 2,
+      b: {
+        c: 3,
+      },
+      c: 5,
+      d: 4,
+    }
+
+    const fixtrueResult = deepMerge(fixtureOrigin, fixturePatch)
+    expect(fixtrueResult).toEqual(fixtureExpected)
+  })
+})
